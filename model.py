23d1a78e53fe7428b3d9c79f5817d4dfe4d4db5f
Pinky-Chaudhary <46152393+Pinky-Chaudhary@users.noreply.github.com>
2021-04-04 04:35:07 +0530

    Update model.py

--- model.py
+++ model.py
@@ -49,7 +49,7 @@ def connect_to_db(app):
 
     # Configure to use PostgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///modeldb'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.app = app
     db.init_app(app)
 
