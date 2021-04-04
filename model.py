f4ae892bba271a27714c3ce009b667350a19b935
Pinky-Chaudhary <46152393+Pinky-Chaudhary@users.noreply.github.com>
2021-04-04 16:53:07 +0530

    Update model.py

--- model.py
+++ model.py
@@ -20,7 +20,6 @@ class Track(db.Model):
     uri = db.Column(db.String, primary_key=True)
     id = db.Column(db.String)
     name = db.Column(db.String)
-    danceability = db.Column(db.Numeric(4,3))
     energy = db.Column(db.Numeric(4,3))
     valence = db.Column(db.Numeric(4,3))
     users = db.relationship('User', secondary = 'user_track', backref = 'tracks')
@@ -39,18 +38,17 @@ class UserTrack(db.Model):
     track_uri = db.Column(db.String, db.ForeignKey('tracks.uri'), nullable=False)
 
 
-# def connect_to_db(app):
-#     """Connect the database to app."""
+def connect_to_db(app):
+    """Connect the database to app."""
 
-#     # Configure to use PostgreSQL database
-#     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#     db.app = app
-#     db.init_app(app)
+    # Configure to use PostgreSQL database
+    db.app = app
+    db.init_app(app)
 
-# if __name__ == "__main__":
-#     # For interactive mode
-#     from views import app
+if __name__ == "__main__":
+    # For interactive mode
+    from views import app
     
-#     connect_to_db(app)
-#     db.create_all()
-#     print("Connected to DB.")
+    connect_to_db(app)
+    db.create_all()
+    print("Connected to DB.")
