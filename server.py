350ed6492d3526cebf92a7f03820d3bdf6604ddd
Pinky-Chaudhary <46152393+Pinky-Chaudhary@users.noreply.github.com>
2021-04-04 09:01:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,3 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.secret_key = os.environ['Secret_Key']
-db = SQLAlchemy(app)
-
+from views import app
 if __name__ == '__main__':
     app.run()
